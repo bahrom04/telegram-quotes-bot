75d3bbc211a4a591059234f1e0603c7e02cccd6a
Bahrom <magdiyevbahrom@gmail.com>
2024-01-08 16:31:15 +0500

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-bot = Bot(token='6094429458:AAHqt5UjgoSYXvO8vCzRAKpJ4vUj3I94FqY')
+bot = Bot(token='')
 dp = Dispatcher(bot, storage=storage)
 
 
